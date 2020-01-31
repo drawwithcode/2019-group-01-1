e2ed520513fab77e0cc4057376148913b919f66d
sylathas <abate.niccolo@gmail.com>
2020-01-31 01:44:07 +0100

    jk2

--- public/js/sketch_game.js
+++ public/js/sketch_game.js
@@ -8,6 +8,16 @@ var socket, canvas, givepresent, alfa = 255;
 //Define all the variables for the preload (images and position)
 var position, yourpresent, importedpresent, database, lobster;
 
+var myLat; //define the position of the user and it's coordinates
+var myLon;
+
+var regali = []; //Define arrays holding sent and recieved presents
+var regalimported = [];
+var i = 0; //Define index for your sent presents
+
+var menu = 0; //Define Variables that stops interactions when a menu is open
+var menuOn = false;
+
 //create a map and load it from mapbox
 var myMap;
 var mappa = new Mappa('MapboxGL', "pk.eyJ1IjoiZWxsbGxhYXMiLCJhIjoiY2sybWU2c3JhMGZudTNvcDB0MzIybjM1ZiJ9.QEanQ7AjFqMkaCCghI-qig");
@@ -30,16 +40,6 @@ function preload() {
   database = loadJSON("../presents.json");
   lobster = loadFont('./assets/fonts/Lobster-Regular.ttf');
 }
-
-var myLat; //define the position of the user and it's coordinates
-var myLon;
-
-var regali = []; //Define arrays holding sent and recieved presents
-var regalimported = [];
-var i = 0; //Define index for your sent presents
-
-var menu = 0; //Define Variables that stops interactions when a menu is open
-var menuOn = false;
 
 function setup() {
   //Create a canvas element and give it "Canvas" class
@@ -69,7 +69,7 @@ function setup() {
 
   //Create Send Present button
   givepresent = createImg("assets/gif/button.gif");
-  givepresent.position(width / 2 - width/6, height / 1.3);
+  givepresent.position(width / 2 - width / 6, height / 1.3);
   givepresent.addClass("PresentButton");
 
   //Scroll through the JSON to place every present in that list on your map
@@ -99,8 +99,8 @@ function draw() {
   alfa = alfa - 3;
   noFill()
   stroke(255, 0, 0, alfa); //Transparency lessens over time
-  ellipse(myPosition.x, myPosition.y, 255 - alfa);  //Radius increases over time
-  if (alfa < 0){
+  ellipse(myPosition.x, myPosition.y, 255 - alfa); //Radius increases over time
+  if (alfa < 0) {
     alfa = 255; //If alfa goes under 0 put it back to 255 to restart radar animation
   }
   pop();
@@ -120,13 +120,19 @@ function draw() {
   if (menu === 1) {
     push();
     if (menuOn === false) {
-      $("#MenuPresent").css({"zIndex": "10", "opacity": "1"});
+      $("#MenuPresent").css({
+        "zIndex": "10",
+        "opacity": "1"
+      });
       $("#Question1").css("opacity", "1");
       $("#Send").css("opacity", "1");
       $("#QuestionName").css("opacity", "1");
       $("#back").css("opacity", "1");
       $("#answer1").css("opacity", "0");
-      $("#illustration").css({"pointerEvents": "none", "opacity": "0"});
+      $("#illustration").css({
+        "pointerEvents": "none",
+        "opacity": "0"
+      });
       menuOn = true;
     }
     pop();
@@ -136,19 +142,25 @@ function draw() {
   if (menu === 2) {
     push();
     if (menuOn === false) {
-      $("#MenuPresent").css({"zIndex": "10", "opacity": "1"});
+      $("#MenuPresent").css({
+        "zIndex": "10",
+        "opacity": "1"
+      });
       $("#Question1").css("opacity", "0");
       $("#QuestionName").css("opacity", "0");
       $("#back").css("opacity", "0");
       $("#Send").css("opacity", "0");
       $("#answer1").css("opacity", "1");
-      $("#illustration").css({"pointerEvents": "auto", "opacity": "1"});
+      $("#illustration").css({
+        "pointerEvents": "auto",
+        "opacity": "1"
+      });
       //Choose random illustration by mood of the user that sent the present
-      if(document.getElementById("answer1").innerHTML === "I'm not very good"){
-        $("#illustration").css("background-image", "url(assets/gif/illustration"+round(random(1)+3)+".gif)");
-      } else if(document.getElementById("answer1").innerHTML === "I'm as usual"){
-        $("#illustration").css("background-image", "url(assets/gif/illustration"+round(random(1)+1)+".gif)");
-      } else if(document.getElementById("answer1").innerHTML === "I'm great!"){
+      if (document.getElementById("answer1").innerHTML === "I'm not very good") {
+        $("#illustration").css("background-image", "url(assets/gif/illustration" + round(random(1) + 3) + ".gif)");
+      } else if (document.getElementById("answer1").innerHTML === "I'm as usual") {
+        $("#illustration").css("background-image", "url(assets/gif/illustration" + round(random(1) + 1) + ".gif)");
+      } else if (document.getElementById("answer1").innerHTML === "I'm great!") {
         $("#illustration").css("background-image", "url(assets/gif/illustration5.gif)");
       }
       menuOn = true;
@@ -163,15 +175,20 @@ function draw() {
 }
 
 //Touch events (used only becouse in the Iphone the same function in mouseClicked didn't work)
-function touchStarted(){
+function touchStarted() {
 
   //Send Menu disappears when the "Send" button is pressed
-  if (menu === 1 && mouseX > width*2.85/8 && mouseX < width * 5.3/8 && mouseY > height*4.4/8 && mouseY < height*5.5/8) {
-    if(document.getElementById('sad').checked || document.getElementById('normal').checked || document.getElementById('happy').checked){
+  if (menu === 1 && mouseX > width * 2.85 / 8 && mouseX < width * 5.3 / 8 && mouseY > height * 4.4 / 8 && mouseY < height * 5.5 / 8) {
+    if (document.getElementById('sad').checked || document.getElementById('normal').checked || document.getElementById('happy').checked) {
       GivePresent();
       menu = 0;
-      $("#MenuPresent").css({"zIndex": "0", "opacity": "0"});
-      $("#illustration").css({"pointerEvents": "none"});
+      $("#MenuPresent").css({
+        "zIndex": "0",
+        "opacity": "0"
+      });
+      $("#illustration").css({
+        "pointerEvents": "none"
+      });
       menuOn = false;
       givepresent.style("opacity", "1", "pointerEvents", "auto");
     }
@@ -181,22 +198,34 @@ function touchStarted(){
 //Touch events
 function mouseClicked() {
   //Go back from Send Menu to Map by touching everywhere else on the screen
-  if(menu === 1 && menuOn === true){
-    if(mouseY > height*3/4 || mouseY < height/4){
+  if (menu === 1 && menuOn === true) {
+    if (mouseY > height * 3 / 4 || mouseY < height / 4) {
       menu = 0;
-      $("#MenuPresent").css({"zIndex": "0", "opacity": "0"});
-      $("#illustration").css({"pointerEvents": "none"});
+      $("#MenuPresent").css({
+        "zIndex": "0",
+        "opacity": "0"
+      });
+      $("#illustration").css({
+        "pointerEvents": "none"
+      });
       givepresent.style("opacity", "1", "pointerEvents", "auto");
-      setTimeout(function(){menuOn = false;}, 300);
+      setTimeout(function() {
+        menuOn = false;
+      }, 300);
     }
   }
 
   //Go back from Recieve Menu to Map by touching everywhere on the screen
   if (menu === 2 && menuOn === true) {
     menu = 0;
-    $("#MenuPresent").css({"zIndex": "0", "opacity": "0"});
+    $("#MenuPresent").css({
+      "zIndex": "0",
+      "opacity": "0"
+    });
     givepresent.style("opacity", "1", "pointerEvents", "auto");
-    setTimeout(function(){menuOn = false;}, 300);
+    setTimeout(function() {
+      menuOn = false;
+    }, 300);
   }
 
   //Make present disappear after clicked
@@ -206,7 +235,7 @@ function mouseClicked() {
 
   //Make present appear when the "Send" Button is clicked
   var dbutton = dist(mouseX, mouseY, width / 2, height / 1.2);
-  if (dbutton < width / 5 && menuOn  === false && menu === 0) {
+  if (dbutton < width / 5 && menuOn === false && menu === 0) {
     menu = 1;
   }
 }
@@ -329,6 +358,7 @@ function RegaloImported(data) {
       document.getElementById("answer1").innerHTML = "I'm as usual";
     } else if (question1 === 3) {
       document.getElementById("answer1").innerHTML = "I'm Great!";
+    }
   }
 }
 
@@ -347,7 +377,7 @@ function stopIcon(stopIcon) {
   regalimported = []; //deletes all the previus shown presents
   database = loadJSON("../presents.json"); //Loads JSON with new informations
   //After some time to ensure that the JSON is loaded recreate all the presents Objects
-  setTimeout(function(){
+  setTimeout(function() {
     for (var t = 0; t < database.regali.length; t++) {
       var data = {
         x: database.regali[t].x,
@@ -370,7 +400,7 @@ function stopIcon(stopIcon) {
 function leavePresent(request) {
   regalimported = []; //As last function
   database = loadJSON("../presents.json");
-  setTimeout(function(){
+  setTimeout(function() {
     for (var t = 0; t < database.regali.length; t++) {
       var data = {
         x: database.regali[t].x,
@@ -381,7 +411,7 @@ function leavePresent(request) {
       }
       regalimported[t] = new RegaloImported(data);
     }
-  },1000);
+  }, 1000);
 }
 
 //Change of the user position on movement
@@ -408,8 +438,14 @@ function touchEnded(event) {
 // =                LOAD AND GOTO ANIMATION                    =
 // =============================================================
 
-$( document ).ready(function(){
-  setTimeout(function(){$("#g").addClass("animateload");}, 1600);
-  setTimeout(function(){$("#b").addClass("animateload");}, 1800);
-  setTimeout(function(){$("#r").addClass("animateload");}, 2000);
+$(document).ready(function() {
+  setTimeout(function() {
+    $("#g").addClass("animateload");
+  }, 1600);
+  setTimeout(function() {
+    $("#b").addClass("animateload");
+  }, 1800);
+  setTimeout(function() {
+    $("#r").addClass("animateload");
+  }, 2000);
 });
